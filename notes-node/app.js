2457826f21cd3dd0d74d6a7669a7f0ee0f514e73
LBerrocal <LBerrocal@pancanal.com>
2017-11-18 22:22:59 -0500

    Installed yards

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,16 +2,19 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+
 const notes = require('./notes.js');
+
 const _ = require('lodash');
-var user = os.userInfo();
+const yargs = require('yargs');
 
-var res = notes.addNote('Si', 'Se puede');
-var unique = _.uniq(['Luis', 1, 'mike', 1, 2, 3]);
-console.log(unique);
 
-fs.appendFile('output/greetings.txt', `Hello ${user.username}! You are ${notes.age}`, function(err){
-   if (err) {
-       console.log('Unable to write to file');
-   };
-});
+args = yargs.argv;
+
+command = args._[0];
+
+console.log(args);
+
+if (command === 'add'){
+    notes.addNote(args.title, args.body)
+}
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,6 +1,10 @@
 console.log('Starting notes.js');
 
-module.exports.addNote = (x, y) => {
-    console.log(`Added a note with ${x} and ${y}` );
-    return `Note with ${x} and ${y}`;
+var addNote = (title, body) => {
+    console.log('Adding note', title, body)
+};
+
+
+module.exports = {
+    addNote
 };
