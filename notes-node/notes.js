603b3a6751912cd5ed774e113813ccf5719bd44b
LBerrocal <LBerrocal@pancanal.com>
2017-11-19 08:18:36 -0500

    Adding notes to json file

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,7 +1,27 @@
 console.log('Starting notes.js');
 
+const fs = require('fs')
+
 var addNote = (title, body) => {
-    console.log('Adding note', title, body)
+    var sourceFile = './output/notes-data.json';
+    var notes = [];
+    var note = {
+        title,
+        body
+    };
+    try {
+        notesString = fs.readFileSync(sourceFile);
+        notes = JSON.parse(notesString);
+    } catch (e) {
+
+    }
+
+    var duplicateNotes = notes.filter((notes) => note.title === title);
+
+    if (duplicateNotes.length === 0){
+        notes.push(note);
+        fs.writeFileSync(sourceFile, JSON.stringify(notes));
+    }
 };
 
 var listNotes = () => {
