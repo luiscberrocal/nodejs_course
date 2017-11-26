3483430dec55607386683e8a9201795375767491
LBerrocal <LBerrocal@pancanal.com>
2017-11-26 05:42:54 -0500

    HBS partials

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => {
-   res.send('Hello!!');
+app.get('/', (req, res) =>{
+    res.render('home.hbs', {
+        title: 'Hello',
+        name: 'Luis',
+        year: new Date().getFullYear()
+    })
+});
+
+app.get('/about', (req, res) =>{
+    res.render('about.hbs', {
+        title: 'About page',
+        year: new Date().getFullYear()
+    })
 });
 
 
@@ -14,4 +30,7 @@ app.get('/bad', (req, res) => {
        errorMessage: 'Could not process request'
    });
 });
+
 app.listen(3000);
+
+console.log('Server up listening to port 3000');
